bb8170d62f55f737ac85af12a56ec5a4003ee330
lanzhiheng <lanzhihengrj@gmail.com>
2018-05-31 21:53:52 +0800

    Change expt file name and use sum method to preduce multiply

---
+++ Chapter1/multi-by-sum.rkt
@@ -0,0 +1,19 @@
+;; import the counter
+(require "../share/step-counter.rkt")
+
+(define (double x) (* 2 x))
+
+(define (halve x) (/ x 2))
+
+(define (multiply a b)
+  (increment!)
+  (if (= b 0)
+      0
+      (+ a (multiply a (- b 1)))))
+
+
+(define (fast-multiply a b)
+  (increment!)
+  (cond [(= b 0) 0]
+        [(even? b) (fast-multiply (double a) (halve b))]
+        [else (+ a (fast-multiply a (- b 1)))]))
